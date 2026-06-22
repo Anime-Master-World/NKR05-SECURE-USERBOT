11a1fed164c4a1275a77a092b970ebfe7b10a877
Anime-Master-World <122656597+Anime-Master-World@users.noreply.github.com>
2026-06-22 06:39:16 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+STRING_SESSION = os.getenv("STRING_SESSION")
+
+MONGO_URI = os.getenv("MONGO_URI")
+
+OWNER_ID = int(os.getenv("OWNER_ID"))
+
+LOG_CHAT_ID = int(os.getenv("LOG_CHAT_ID"))
+
+PREFIX = "."
